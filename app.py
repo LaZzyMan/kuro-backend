ad2f5c88a22a538d8fa74869f2ca7d313429f3c7
lazzy <zeusdream7@gmail.com>
2022-01-13 14:58:25 +0800

    训练API

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template
 from threading import Lock
 from flask_socketio import SocketIO
 from model import KuroModel
+import numpy as np
 
 app = Flask(__name__)
 
@@ -10,6 +11,8 @@ socketio.init_app(app, cors_allowed_origins='*', async_mode=None)
 thread = None
 thread_lock = Lock()
 kuro_ns = '/kuro'
+
+adjs = np.load('./large_data/adjs.npy')
 
 
 @app.route('/')
@@ -31,6 +34,10 @@ def disconnect_msg():
     socketio.emit('disconnect', {'msg': 'Disconnected.'}, namespace=kuro_ns)
     print('client disconnected.')
 
+@socketio.on('adjs', namespace=kuro_ns)
+def get_adj(index):
+    socketio.emit('adj', {'in': adjs[:, :, index].tolist(), 'out': adjs[:, index].tolist()}, namespace=kuro_ns)
+
 @socketio.on('train', namespace=kuro_ns)
 def kuro_train(train_set):
     global thread
@@ -48,7 +55,7 @@ def train_model(train_set):
     socketio.emit('train_info', {'type': 'info', 'content': 'Loading Data.'}, namespace=kuro_ns)
     model.load_data()
     model.create_model()
-    model.train(model.default_train_set, onEpochEnd)
+    model.train(train_set, onEpochEnd)
     socketio.emit('train_result', {'type': 'info', 'content': 'Train Finished.'}, namespace=kuro_ns)
     results = model.prediction(model.default_train_set)
     socketio.emit('train_result', {'type': 'result', 'content': results}, namespace=kuro_ns)
